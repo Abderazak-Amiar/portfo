9cdf7f799514a6ba024c67e2175a97dced5c4143
Abderazak-Amiar <amiar.abderazak@gmail.com>
2023-05-13 02:21:58 +0100

    create Admin page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,62 @@
 import React from "react"
-import Header from "./Header";
-import Root from "../routes/Root";
-import Contact from "../routes/Contact";
-import About from "../routes/About";
-import Portfolio from "../routes/Portfolio";
-import ErrorPage from "../routes/Error-page";
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
-import Expertise from "../routes/Expertise";
-import Footer from "./Footer";
-
+import Root from "../routes/Root"
+import Contact from "../routes/Contact"
+import About from "../routes/About"
+import Portfolio from "../routes/Portfolio"
+import ErrorPage from "../routes/Error-page"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteMatch,
+} from "react-router-dom"
+import Expertise from "../routes/Expertise"
+import Admin from "../routes/admin"
+import Footer from "./Footer"
+import Header from "./Header"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement : <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
     path: "/portfolio",
     element: <Portfolio />,
-    errorElement : <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
-    path : "/about",
-    element : <About />,
-    errorElement : <ErrorPage />
+    path: "/about",
+    element: <About />,
+    errorElement: <ErrorPage />,
   },
   {
-    path : "/contact",
-    element : <Contact />,
-    errorElement : <ErrorPage />
+    path: "/contact",
+    element: <Contact />,
+    errorElement: <ErrorPage />,
   },
   {
-    path : "/expertise",
-    element : <Expertise />,
-    errorElement : <ErrorPage />
+    path: "/expertise",
+    element: <Expertise />,
+    errorElement: <ErrorPage />,
   },
-  
-]);
+  {
+    path: "/admin",
+    element: <Admin />,
+    errorElement: <ErrorPage />,
+  },
+])
 
+const currentRoute = router.state.location.pathname
+console.log("==>", currentRoute)
 function App() {
-
   return (
-  <React.StrictMode>
-    <Header />
-    <RouterProvider router={router} />
-    <Footer />
-  </React.StrictMode>
-  );
+    <React.StrictMode>
+      {!(currentRoute === "/admin") && <Header />}
+      <RouterProvider router={router}></RouterProvider>
+      {!(currentRoute === "/admin") && <Footer />}
+    </React.StrictMode>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 
+
+
 ReactDOM.createRoot(
   document.getElementById("root"),
 )
