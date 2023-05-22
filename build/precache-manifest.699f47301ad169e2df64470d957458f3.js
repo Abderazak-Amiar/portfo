99f819395859a21e701ce9b3ae30828201b30d93
Abderazak-Amiar <amiar.abderazak@gmail.com>
2023-05-22 23:15:57 +0100

    API URL CONFIG

---
+++ build/precache-manifest.699f47301ad169e2df64470d957458f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63dac8c2032fdbcd07d3",
+    "url": "/static/js/main.63dac8c2.chunk.js"
+  },
+  {
+    "revision": "93d49601ddd60274c8f8",
+    "url": "/static/css/1.c9aec78f.chunk.css"
+  },
+  {
+    "revision": "93d49601ddd60274c8f8",
+    "url": "/static/js/1.93d49601.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03b1d4dcce7abca8b0572b24773048ec",
+    "url": "/index.html"
+  }
+];
